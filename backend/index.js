7d930bda3ea7d15caf0407efcbc6909e5256b4a7
samar2605 <bm20btech11012@iith.ac.in>
2024-06-02 21:48:19 +0530

    Back to local for now

--- backend/index.js
+++ backend/index.js
@@ -9,13 +9,13 @@ app.use(cors());
 // Database configuration
 const { Sequelize } = require('sequelize');
 
-//for local
-// const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
-//   host: process.env.DB_HOST,
-//   dialect: process.env.DB_DIALECT,
-// });
+// for local
+const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: process.env.DB_DIALECT,
+});
 
-const sequelize = new Sequelize(process.env.MYSQL_URL, {dialect:"mysql",logging: console.log} );
+// const sequelize = new Sequelize(process.env.MYSQL_URL, {dialect:"mysql",logging: console.log} );
 
 // Test database connection
 sequelize.authenticate()
